ba3806b238bc483cd5818ac68d441325dd944d44
gabriel almendarez <galmendarez4896@gmail.com>
2018-05-08 23:39:49 -0700

    added coffeescript 2 support

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,17 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.coffee',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.coffee$/,
+        use: [ 'coffee-loader' ]
+      }
+    ]
   }
 };
